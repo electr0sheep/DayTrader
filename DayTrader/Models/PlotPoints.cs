6d5d6b651430765f948aa053f15b692de8753c5d
electr0sheep <electr0sheep@electr0sheep.com>
2024-08-19 21:58:17 -0600

    Plot points need to be ordered on x axis

--- DayTrader/Models/PlotPoints.cs
+++ DayTrader/Models/PlotPoints.cs
@@ -8,28 +8,43 @@ namespace DayTrader.Models
 {
     internal class PlotPoints
     {
-        private readonly List<float> xs = [];
-        private readonly List<float> ys = [];
+        private List<float> xs = [];
+        private List<float> ys = [];
+        private readonly SortedList<float, float> points = [];
 
         public void Add(DateTimeOffset x, float y)
         {
-            this.xs.Add(x.ToUnixTimeSeconds());
-            this.ys.Add(y);
+            //this.xs.Add(x.ToUnixTimeSeconds());
+            //this.ys.Add(y);
+            //Service.PluginLog.Debug($"Adding point ({x.ToUnixTimeSeconds()},{y})");
+            this.points.Add(x.ToUnixTimeSeconds(), y);
         }
 
         public float[] GetXs()
         {
+            xs = [];
+            foreach (var pair in points)
+            {
+                xs.Add(pair.Key);
+            }
+            //return [.. this.points.Values];
             return [.. this.xs];
         }
 
         public float[] GetYs()
         {
+            ys = [];
+            foreach (var pair in points)
+            {
+                ys.Add(pair.Value);
+            }
+            //return [.. this.points.Keys];
             return [.. this.ys];
         }
 
         public int GetSize()
         {
-            return this.xs.Count;
+            return this.points.Count;
         }
     }
 }
